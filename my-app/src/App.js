1c62bcb0438156ef1474051d95e8da21990da78b
Gman-root <gpandey2020@gmail.com>
2023-11-05 01:14:30 -0500

    line chart

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 function App() {
+  const [monthlyPayment, setMonthlyPayment] = useState(1000);
+  const [data, setData] = useState([]);
+
+  const calculateSavings = (monthlyPayment) => {
+    const interestRate = 0.05;
+    const years = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const savingsData = years.map(year => {
+      const months = year * 12;
+      let balance = 0;
+      for (let i = 0; i < months; i++) {
+        balance += monthlyPayment;
+        balance *= (1 + interestRate / 12);
+      }
+      const savings = balance - (monthlyPayment * months);
+      return { year, savings };
+    });
+    setData(savingsData);
+  }
+
+  const handleSliderChange = (event) => {
+    const value = event.target.value;
+    setMonthlyPayment(value);
+    calculateSavings(value);
+  }
+
+  useEffect(() => {
+    calculateSavings(monthlyPayment);
+  }, [monthlyPayment]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit Gaurav <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Money Saved Over Time</h1>
+      <p>Monthly Payment: ${monthlyPayment}</p>
+      <input type="range" min="0" max="10000" value={monthlyPayment} onChange={handleSliderChange} />
+      <LineChart width={600} height={300} data={data}>
+        <XAxis dataKey="year" />
+        <YAxis />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="savings" stroke="#8884d8" />
+      </LineChart>
     </div>
   );
 }
